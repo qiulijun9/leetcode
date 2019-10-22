d2d6f0ee05b331e29cb804845e5fd494be7fae74
仇丽君 <1424626027@qq.com>
2019-10-22 22:58:42 +0800

    二进制求和

---
+++ 数组/1022/二进制求和.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let maxLength = Math.max(a.length, b.length) + 1;
+  while (a.length !== maxLength) a = "0" + a;
+  while (b.length !== maxLength) b = "0" + b;
+  var c = [];
+  for (var i = maxLength - 1; i >= 0; i--) {
+    c[i] = c[i] ? c[i] + (a[i] - 0) + (b[i] - 0) : a[i] - 0 + (b[i] - 0);
+    if (c[i] > 1) {
+      c[i - 1] = c[i - 1] ? c[i - 1] + 1 : 1;
+      c[i] = c[i] - 2;
+    }
+  }
+  if (!c[0]) c.shift();
+  return c.join("");
+};

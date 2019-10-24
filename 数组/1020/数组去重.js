cc6bfc4509c806b11a5a2f5469f7ba54e16e578e
仇丽君 <1424626027@qq.com>
2019-10-24 23:25:55 +0800

    两数之和

--- 数组/1020/数组去重.js
+++ 数组/1020/数组去重.js
@@ -4,9 +4,23 @@
  */
 var containsDuplicate = function(nums) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < i; ++j) {
+    for (let j = 0; j < i; j++) {
       if (nums[j] == nums[i]) return true;
     }
   }
   return false;
 };
+
+var containsDuplicate2 = function(nums) {
+  const hashMap = {};
+  return nums.some(num => {
+    if (hashMap[num]) {
+      return true;
+    } else {
+      hashMap[num] = true;
+      return false;
+    }
+  });
+};
+
+containsDuplicate2([2, 34, 54, 54, 3, 2, 3]);

25565b4a8c5d3d357b25ef73c7ab314ac59fd6d6
qiulijun <qiulijun9@aliyun.com>
2021-02-13 20:58:28 +0800

    feat:98 add 递归

--- 0-100/098.验证二叉搜索树.js
+++ 0-100/098.验证二叉搜索树.js
@@ -33,3 +33,69 @@ var isValidBST = function (root) {
   }
   return true
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  let preRoot = -Infinity
+  let result = true
+
+  // 中序遍历
+  function helper(root) {
+    if (!root) {
+      return
+    }
+    helper(root.left) //左
+    // 中
+
+    if (root.val <= preRoot) {
+      result = false
+      return
+    }
+    preRoot = root.val
+    // 右
+    helper(root.right)
+  }
+
+  helper(root)
+
+  return result
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  return helper(root, -Infinity, Infinity)
+}
+// 递归
+function helper(root, min, max) {
+  if (root === null) {
+    return true
+  }
+  if (min !== null && root.val <= min) {
+    return false
+  }
+  if (max !== null && root.val >= max) {
+    return false
+  }
+
+  return helper(root.left, min, root.val) && helper(root.right, root.val, max)
+}

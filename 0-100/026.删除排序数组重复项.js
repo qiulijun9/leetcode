29cd90a1969664010d04b44f9a65768ed77f0229
qiulijun <qiulijun9@aliyun.com>
2021-03-18 16:28:14 +0800

    feat:快慢指针 26

--- 0-100/026.删除排序数组重复项.js
+++ 0-100/026.删除排序数组重复项.js
@@ -2,15 +2,42 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-  let cur = nums[0];
+var removeDuplicates = function (nums) {
+  let cur = nums[0]
 
   for (let i = 1; i < nums.length; ) {
     if (nums[i] === cur) {
-      nums.splice(i, 1);
+      nums.splice(i, 1)
     } else {
-      cur = nums[i++];
+      cur = nums[i++]
     }
   }
-  return nums.length;
-};
+  return nums.length
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  // 设置快慢指针
+  // left 比 fast 快 异步， 快指针先走一步，当发现值不同 slow ++ ,替换该值 nums[slow] = nums[fast] 如果值重复 fast ++
+  // 结果为 slow +1
+
+  if (!nums.length) {
+    return 0
+  }
+
+  let slow = 0
+  let fast = 1
+
+  while (fast < nums.length) {
+    if (nums[slow] !== nums[fast]) {
+      slow++
+      nums[slow] = nums[fast]
+    }
+
+    fast++
+  }
+  return slow + 1
+}

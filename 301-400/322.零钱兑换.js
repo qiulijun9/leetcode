82cc0ec2ca5ef1163915a6b4b6cc39c46ab7e99a
qiulijun <qiulijun9@aliyun.com>
2021-03-25 10:50:19 +0800

    feat:add 322

--- 301-400/322.零钱兑换.js
+++ 301-400/322.零钱兑换.js
@@ -6,7 +6,7 @@
 // var coinChange = function(coins, amount) {
 //   let dp = new Array( amount + 1 ).fill( Infinity );
 //   dp[0] = 0;
-  
+
 //   for (let i = 1; i <= amount; i++) {
 //     for (let coin of coins) {
 //       if (i - coin >= 0) {
@@ -14,25 +14,39 @@
 //       }
 //     }
 //   }
-  
+
 //   return dp[amount] === Infinity ? -1 : dp[amount];
 // };
 
+var coinChange = function (coins, amount) {
+  function dp(n) {
+    if (n === 0) return 0
+    if (n < 0) return -1
 
-var coinChange = function(coins, amount) {
-
-
-  function dp(n){
-    if (n === 0) return 0;
-    if (n < 0) return -1;
-
-    let result =0;
-    for(let i=0; i< coins.length; i++){
-      let subproblem = dp(n- conis[i]);
-      result = Math.min(result,subproblem+1);
+    let result = 0
+    for (let i = 0; i < coins.length; i++) {
+      let subproblem = dp(n - coins[i])
+      result = Math.min(result, subproblem + 1)
     }
-   
   }
 
   return dp(amount)
+}
+
+// 动态规划，dp 方程 ： dp[i] = Math.min(dp[i],dp[i- coins[j]]+1)
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(amount + 1)
+
+  dp[0] = 0
+
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      // 硬币的值小于求和的这个值才
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1)
+      }
+    }
+  }
+
+  return dp[amount] > amount ? -1 : dp[amount]
 }
--- 算法/二分查找.js
+++ 算法/二分查找.js
@@ -1,16 +1,16 @@
-function binarySearch (nums, target){
-  let left = 0;
-  let right = nums.length -1; 
-  while(left <= right){
-    let mid = Math.floor((right - left)/2) + left;
-    if(nums[mid] === target) return mid;
-    if(nums[mid] < target){
-      left = mid + 1; //区间变成[mid+1,right]
+function binarySearch(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  while (left <= right) {
+    let mid = Math.floor((right - left) / 2) + left
+    if (nums[mid] === target) return mid
+    if (nums[mid] < target) {
+      left = mid + 1 //区间变成[mid+1,right]
     }
-    if(nums[mid] > target){
-      right = mid - 1;//区间变成[left,mid-1]
+    if (nums[mid] > target) {
+      right = mid - 1 //区间变成[left,mid-1]
     }
   }
-  return -1;
+  return -1
 }
-console.log(binarySearch([1,2,3,4,5,6],4))
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4))

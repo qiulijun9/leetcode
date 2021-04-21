9743f377e54fa89572f89712d7f09671414fa525
qiulijun <qiulijun9@aliyun.com>
2021-04-21 15:46:34 +0800

    chore: 重新理解226

--- 201-300/226.翻转二叉树.js
+++ 201-300/226.翻转二叉树.js
@@ -23,3 +23,25 @@ var invertTree = function (root) {
   invertTree(root.right)
   return root
 }
+
+var invertTree = function (root) {
+  // 明确root 节点需要做什么
+  // 翻转二叉树 ,也就是交换左右子树上的值， 那就需要一个变量来进行交换
+
+  if (root === null) {
+    return null
+  }
+  const temp = root.left
+  root.left = root.right
+  root.right = temp
+
+  //  子节点进行递归,做相同的事情
+  invertTree(root.left)
+  invertTree(root.right)
+
+  return root
+}
+
+// 左右根  312 967 --- 472 96 31
+// 根左右  472  312 967 -- 472 96 31
+// 左根右  312  472  213 967 - 472 13 69       4721369

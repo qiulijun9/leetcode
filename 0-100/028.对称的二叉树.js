2833a6d0b28cb018e4ca353c71e77cd146c21e56
qiulijun <qiulijun9@aliyun.com>
2021-04-28 10:19:06 +0800

    feat: add 028

--- 0-100/028.对称的二叉树.js
+++ 0-100/028.对称的二叉树.js
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
 var isSymmetric = function (root) {
   if (root === null) {
     return true
@@ -32,4 +21,37 @@ function helper(leftNode, rightNode) {
     helper(leftNode.left, rightNode.right) &&
     helper(leftNode.right, rightNode.left)
   )
+}
+
+var isSymmetric = function (root) {
+  let queue = []
+  if (root === null) {
+    return true
+  }
+  queue.push(root.left)
+  queue.push(root.right)
+
+  while (queue.length) {
+    const leftNode = queue.shift()
+    const rightNode = queue.shift()
+
+    if (leftNode === null && rightNode === null) {
+      continue
+    }
+
+    if (
+      leftNode === null ||
+      rightNode === null ||
+      leftNode.val !== rightNode.val
+    ) {
+      return false
+    }
+
+    queue.push(leftNode.left)
+    queue.push(rightNode.right)
+
+    queue.push(leftNode.right)
+    queue.push(rightNode.left)
+  }
+  return true
 }

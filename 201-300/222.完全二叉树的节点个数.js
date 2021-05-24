adace4c0454952543392c48fb75b627304211fe8
qiulijun <qiulijun9@aliyun.com>
2021-05-24 11:00:24 +0800

    feat: add 222

--- 201-300/222.完全二叉树的节点个数.js
+++ 201-300/222.完全二叉树的节点个数.js
@@ -37,3 +37,68 @@ var countNodes = function (root) {
   // 普通的二叉树
   return 1 + countNodes(root.left) + countNodes(root.right)
 }
+
+// 超出时间限制
+var countNodes = function (root) {
+  let result = []
+  if (root === null) {
+    return 0
+  }
+  traversal(root, result)
+  return result.length
+}
+
+function traversal(root, result) {
+  console.log(result)
+  if (root === null) {
+    return null
+  }
+
+  result.push(root.val)
+
+  traversal(root.left, result)
+  traversal(root.right, result)
+}
+
+//dfs
+var countNodes = function (root) {
+  if (root === null) {
+    return 0
+  }
+
+  return countNodes(root.left) + countNodes(root.right) + 1
+}
+
+// 利用完全二叉树的性质
+// 普通二叉树和 满二叉树的结合版
+
+var countNodes = function (root) {
+  let leftH = 0
+  let rightH = 0
+  let left = root
+  let right = root
+
+  if (root === null) {
+    return 0
+  }
+
+  while (left !== null) {
+    leftH++
+    left = left.left
+  }
+
+  while (right !== null) {
+    rightH++
+    right = right.right
+  }
+
+  // 是满二叉树时,左子树的高 和右子树的高相同
+
+  if (leftH === rightH) {
+    return Math.pow(2, leftH) - 1
+  }
+
+  // 普通的二叉树
+
+  return countNodes(root.left) + countNodes(root.right) + 1
+}

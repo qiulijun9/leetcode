42463de2637587fa140e010ec451758868433bc2
qiulijun <qiulijun9@aliyun.com>
2021-06-20 19:42:22 +0800

    feat:add 191

---
+++ 101-200/191.位1的个数.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ *
+ * x =x &(x-1) 目的消除末尾的1
+ */
+var hammingWeight = function (n) {
+  let count = 0
+
+  while (n !== 0) {
+    count++
+    n = n & (n - 1)
+  }
+
+  return count
+}
+
+var hammingWeight2 = function (n) {
+  let str = n.toString(2)
+  const strSplit = str.split('1')
+  return strSplit.length - 1
+  console.log(999, str, strSplit)
+}
+console.log(hammingWeight2(00000000000000000000000000001011))
